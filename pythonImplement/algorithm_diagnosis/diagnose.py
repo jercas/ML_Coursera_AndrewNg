a0b70c153f860660bdfdd890ede7fedd079957b1
JerCas Ety <jercas0618@163.com>
2018-01-25 16:56:50 +0800

    Building the algorithm diagnose architecture and amplify the range of .gitigore to some useless configuration files.

---
+++ pythonImplement/algorithm_diagnosis/diagnose.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# algorithm_diagnosis/diagnose.py
+"""
+Created on Thu Jan 25 16:10:00 2018
+
+@author: jercas
+"""
---
+++ pythonImplement/algorithm_diagnosis/preprocess.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# algorithm_diagnosis/preprocess.py
+"""
+Created on Thu Jan 25 16:11:00 2018
+
+@author: jercas
+"""
+import linear_regression
+import numpy as np
+from scipy.io import loadmat
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
+
